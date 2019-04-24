aefece52f235c32bb4f5ccadc797a4fb2f4bd989
timichal <michal@zlatkovsky.cz>
2019-04-24 11:19:57 +0200

    href fix 3

--- js/script.js
+++ js/script.js
@@ -37,14 +37,14 @@ function makeTable() {
         <span class="strana">${data[per].partaj}</span>
         <span class="supplemental">${data[per].povolani}</span>
         ${data[per].pozn != null ? `<div><span class="supplemental">${data[per].pozn}</span></div>` : ""}
-        <span class="share">Sdílet na <a class="share-link" href="#" data-share-link=${`https://www.facebook.com/sharer/sharer.php?u=${window.location.href.split("?")[0]}?${per}`}>Facebook</a> | <a class="share-link" href="#" data-share-link=${`https://twitter.com/home?status=${window.location.href.split("?")[0]}?${per}`}>Twitter</a> | <a target="_blank" href="https://data.irozhlas.cz/eu-vizitky-19/iframes.html#${data[per].file}">Vložit do stránky</a></span>
+        <span class="share">Sdílet na <a class="share-link" href=${`https://www.facebook.com/sharer/sharer.php?u=${window.location.href.split("?")[0]}?${per}`}>Facebook</a> | <a class="share-link" href=${`https://twitter.com/home?status=${window.location.href.split("?")[0]}?${per}`}>Twitter</a> | <a target="_blank" href="https://data.irozhlas.cz/eu-vizitky-19/iframes.html#${data[per].file}">Vložit do stránky</a></span>
         ${data[per].afile !== "x" ? `<div><audio class="player" src="${host}media/audio/${data[per].file}.mp3" preload="none" controls="yes"></audio></div>` : ""}
       </div>
       <div class="left">
         <img width="120" height="180" alt="${data[per].jmeno}" src="${`${host}media/foto/${data[per].file}.jpg`}">
       </div>
     `;
-    // 
+
     outList.appendChild(entry);
   });
 
@@ -53,11 +53,13 @@ function makeTable() {
   bottom.appendChild(outList);
 
   const shareLinks = document.getElementsByClassName("share-link");
-  for (let i = 0; i < shareLinks.length; i++) {
-    shareLinks[i].addEventListener("click", (e) => {
-      e.preventDefault();
-      window.open(e.target.attributes["data-share-link"].textContent, "Sdílení", "width=550,height=450,scrollbars=no");
-    });
+  if (window.innerWidth > 600) {
+    for (let i = 0; i < shareLinks.length; i++) {
+      shareLinks[i].addEventListener("click", (e) => {
+        e.preventDefault();
+        window.open(e.target.href, "Sdílení", "width=550,height=450,scrollbars=no");
+      });
+    }
   }
 
   const players = document.getElementsByClassName("player");
